e6d583f333e0197eaa3873132482f071c7899dd5
James Cho <jamescho7@gmail.com>
2014-05-28 13:28:58 -0400

    hello

---
+++ Assets/Kyle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kyle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Kyle is sleeping");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
