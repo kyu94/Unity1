e797c7efecf2a8b7d0910e588df2f6559287501e
James Cho <jamescho7@gmail.com>
2014-05-28 19:10:28 -0400

    Added basic character controller, finished level implementation

--- Assets/CamMove.cs
+++ Assets/CamMove.cs
@@ -10,6 +10,12 @@ public class CamMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float yAxisValue = Input.GetAxis("Vertical");
+        if (Camera.current != null)
+        {
+            Camera.current.transform.Translate(new Vector3(xAxisValue, yAxisValue, 0));
+        }
 	
 	}
 }
