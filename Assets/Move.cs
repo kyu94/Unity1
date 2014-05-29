26654f8c35bcd8af244555028804f2dc08bc382b
Kyle Yu <ninetailsbankai@gmail.com>
2014-05-29 16:11:31 -0400

    Added enemy behavior and destroy

---
+++ Assets/Move.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public Transform leftAnchor;
+	public Transform rightAnchor;
+	bool direction;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Vector3.Distance (transform.position, leftAnchor.position) <= 1) {
+			direction = true;
+		} else if (Vector3.Distance (transform.position, rightAnchor.position) <= 1) {
+			direction = false;
+		}
+		
+	}
+	
+	void FixedUpdate ()
+	{
+		if (direction) {
+			transform.Translate (Vector3.right * .07f);
+		} else {
+			transform.Translate (Vector3.left  * .07f);
+		}
+	}
+}
