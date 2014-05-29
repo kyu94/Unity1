8a3b42b0e3fa3cce3293c85df1d429c791f2fb55
Vignesh Sivashanmugam <vignesh.siva@gmail.com>
2014-05-29 16:19:39 -0400

    Re-orgazed files. Added art to scene. Fixed gravity room.

---
+++ Assets/Scripts/GravityOff.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityOff : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.tag == "Player") {
+
+			Physics2D.gravity = new Vector2(0, -2f);
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/GravityOn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityOn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		
+		if (other.tag == "Player") {
+			
+			Physics2D.gravity = new Vector2(0, -9.81f);
+			
+		}
+		
+	}
+}
