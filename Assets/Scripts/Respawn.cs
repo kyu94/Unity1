5f504bc077f07b7ec5ba59e96af2945f24bf21f2
James Cho <jamescho7@gmail.com>
2014-05-29 14:00:23 -0400

    Added anti gravity chamber & death

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+
+	public Transform player;
+
+	private Vector3 playerPos;
+
+	// Use this for initialization
+	void Start () {
+
+		playerPos = player.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if(other.tag.Equals("Player")) {
+			other.gameObject.transform.position = playerPos;
+		}
+	}
+}
