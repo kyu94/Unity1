820ec616be12a6c2ebada94c7a630d7dcf25b846
Vignesh Sivashanmugam <vignesh.siva@gmail.com>
2014-05-30 11:59:52 -0400

    Added parts 1 and 2 of the final background

---
+++ Assets/LateralCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LateralCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 front = transform.TransformDirection (Vector3.forward);
+		if(Physics.Raycast(transform.position, fwd, 10) {
+			print ("Cast is working!");
+		}
+
+	}
+}
